928c536a21a0a033bdeebf4022a5d58814d791cb
aleTorres05 <torresalexatlanta@gmail.com>
2021-02-17 17:56:40 -0600

    validate the link

--- amazon_notification/price_check.py
+++ amazon_notification/price_check.py
@@ -7,19 +7,22 @@ HEADERS = ({'User-Agent':
             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/44.0.2403.157 Safari/537.36',
             'Accept-Language': 'en-US, en;q=0.5'})
             
-
+link = "https://www.amazon.com"
 def get_price():
     url = input("Place URL for an amazon product: ")
 
     validate_url = validators.url(url)
     if validate_url:
-        page = requests.get(url, headers=HEADERS)
+        if link in url:
+            page = requests.get(url, headers=HEADERS)
 
-        soup = BeautifulSoup(page.content, "html.parser")
+            soup = BeautifulSoup(page.content, "html.parser")
 
-        span = soup.find("span", attrs={"id":"price_inside_buybox"}).string.strip()
-        return span
+            span = soup.find("span", attrs={"id":"price_inside_buybox"}).string.strip()
+            return span
+        else:
+            return "URL not valid!"
     else:
-        return "URL not valid!"
+            return "Not a URL!"
 
 print(get_price())
